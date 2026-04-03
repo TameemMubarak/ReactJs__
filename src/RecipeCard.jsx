39e3e0d22cd1514b440f62e16b694d0eb8872d1c
TameemMubarak <m.ansarithameem004@gmail.com>
2026-04-03 22:00:38 +0530

    Update RecipeCard.jsx

--- src/RecipeCard.jsx
+++ src/RecipeCard.jsx
@@ -6,6 +6,7 @@ import RecipeFooter from "./Components/RecipeFooter";
     return (
 // the template for the food item details.
      <section className="recipe-card">
+      {/* props properity of react JS is used here and the value are passed as attributes values. */}
             <RecipeHeader name={propss.item_name} time={propss.item_time} region={propss.item_region}/>
             <RecipeBody  />
             <RecipeFooter />
