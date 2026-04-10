d131391caff41464515c2ff0893073b4fd067ff9
TameemMubarak <m.ansarithameem004@gmail.com>
2026-04-10 23:46:47 +0530

    Update RecipeFooter.jsx

--- src/Components/RecipeFooter.jsx
+++ src/Components/RecipeFooter.jsx
@@ -1,7 +1,7 @@
 export default function RecipeFooter(){
     return(
         <footer className="recipe-footer">
-            <small>Found by M.Thameem &copy; since 2005.</small>
+            <small>Found by M.Thameem &copy; since 2004.</small>
         </footer>
     );
 
