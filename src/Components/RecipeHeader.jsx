aad794581b020d1d28f3d801cb0449475c6965c9
TameemMubarak <m.ansarithameem004@gmail.com>
2026-04-10 23:47:28 +0530

    Update RecipeHeader.jsx

--- src/Components/RecipeHeader.jsx
+++ src/Components/RecipeHeader.jsx
@@ -3,7 +3,6 @@ export default function RecipeHeader(props){
           {/* props properity of react JS */}
         <header className="recipe-header">
             <h1>{props.name}</h1>
-          
             <p>Prep Time : {props.time} | Regional : {props.region}</p>
         </header>
     );
